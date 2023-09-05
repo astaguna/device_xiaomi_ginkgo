d83484e266368871f9dc8eb3d87af82d23363510
astaguna <nandoscrz@gmail.com>
2023-09-05 18:03:42 +0700

    init

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_ginkgo-user \
+    lineage_ginkgo-userdebug \
+    lineage_ginkgo-eng
